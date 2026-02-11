bb5af65014dd8c73e4e16e6e89755a6603504db9
George5683 <newtech20111@hotmail.com>
2026-02-11 00:52:26 -0500

    Added basic functionality for receiving commands from remote controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,55 @@
 #include <Arduino.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <IRremote.hpp>
 
-#define LED1_PIN 2
-#define BUTTON_PIN 23
+// Define the LED pin
+const int ledPin = 13; // Most Arduino boards have an onboard LED on pin 13
+const int IR_Transmitter = 10; // Pin for the IR transmitter
+const int IR_Receiver = 9; // Pin for the IR receiver
 
-// Flags 
-volatile bool Button_Flag = false;
-volatile bool IR_Flag = false;
-volatile bool Blink_Flag = false;
-// Task handle
-TaskHandle_t BlinkTaskHandle = NULL;
-TaskHandle_t PowerTaskHandle = NULL;
-TaskHandle_t SerialTaskHandle = NULL;
+void blink(){
+  Serial.println("IR signal received! Blinking LED...");
+  // Turn the LED on
+  digitalWrite(ledPin, HIGH);
+  delay(1000); // Wait for 1 second
 
-// Creating the queue
-QueueHandle_t isrQueue;
-// int value = 1;
-// xQueueSendFromISR(isrQueue, &value, NULL);
-// xQueueReceive(isrQueue, &value, portMAX_DELAY)
-
-// Volatile variables for ISR
-volatile bool taskSuspended = false;
-volatile uint32_t lastInterruptTime = 0;
-const uint32_t debounceDelay = 100; // debounce period
-
-void SerialTask(void *parameter) {
-  if (Blink_Flag) {
-    Serial.println("SerialTask: Blinking is ON");
-  } else {
-    Serial.println("SerialTask: Blinking is OFF");
-  }
-
-  if (Button_Flag) {
-    Serial.println("SerialTask: Button was pressed");
-    Button_Flag = false;
-  }
+  // Turn the LED off
+  digitalWrite(ledPin, LOW);
+  delay(1000); // Wait for 1 second
 }
-
-// Fast ISR to toggle task state
-void IRAM_ATTR buttonISRfast(){
-  BaseType_t higherPriorityTaskWoken = pdFALSE;
-  // Toggle task state
-  taskSuspended = !taskSuspended;
-  if (taskSuspended) {
-    vTaskSuspend(BlinkTaskHandle);
-    Blink_Flag = false;
-  } else {
-    vTaskResume(BlinkTaskHandle);
-    Blink_Flag = true;
-  }
-  // notify SerialTask
-  vTaskNotifyGiveFromISR(SerialTaskHandle, &higherPriorityTaskWoken);
-  if (higherPriorityTaskWoken) {
-    portYIELD_FROM_ISR();
-  }
-}
-
-// ISR for button press
-void IRAM_ATTR buttonISRHandler() {
-  // Debounce the button press
-  uint32_t currentTime = millis();
-  if (currentTime - lastInterruptTime < debounceDelay) {
-    return;
-  }
-  lastInterruptTime = currentTime;
-  buttonISRfast();
-}
-
-void BlinkTask(void *parameter) {
-  for (;;) { // Infinite loop
-    digitalWrite(LED1_PIN, HIGH);
-    Serial.println("BlinkTask: LED ON");
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000ms
-    digitalWrite(LED1_PIN, LOW);
-    Serial.println("BlinkTask: LED OFF");
-    Serial.print("BlinkTask running on core ");
-    Serial.println(xPortGetCoreID());
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    Serial.printf("Task1 Stack Free: %u bytes\n", uxTaskGetStackHighWaterMark(NULL));
-
-  }
-}
-
-void Powerfunction(void *parameter){
-  // Power ON/OFF
+// Allow a menu of devices to control 
+void DeviceMenu(){
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600); // Start serial communication at 9600 baud rate
 
-  Serial.printf("Starting FreeRTOS: Memory Usage\nInitial Free Heap: %u bytes\n", xPortGetFreeHeapSize());
+  // Delay 10 seconds to give time to open the Serial Monitor
+  delay(10000);
 
-  // Initialize pins
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Internal pull-up resistor
+  Serial.println("Starting IR Receiver...");
 
-  // Create task for blinking LED
-  xTaskCreatePinnedToCore(
-    BlinkTask,         // Task function
-    "BlinkTask",       // Task name
-    10000,             // Stack size (bytes)
-    NULL,              // Parameters
-    4,                 // Priority
-    &BlinkTaskHandle,  // Task handle
-    1                  // Core 1
-  );
+  // Initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
 
-  // Create task for using the IR sensor to power ON/OFF televisions
-  xTaskCreatePinnedToCore(
-    Powerfunction,         // Task function
-    "PowerTask",       // Task name
-    10000,             // Stack size (bytes)
-    NULL,              // Parameters
-    5,                 // Priority
-    &PowerTaskHandle,  // Task handle
-    1                  // Core 1
-  );
-
-  xTaskCreatePinnedToCore(
-    SerialTask,
-    "SerialTask",
-    4096,
-    NULL,
-    5,
-    &SerialTaskHandle,
-    1
-  );
-
-  // Attach interrupt to button
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISRHandler, FALLING);
+  // Start the IR transmitter on Sensor Pin
+  IrReceiver.begin(IR_Receiver, ENABLE_LED_FEEDBACK);  
 }
 
 void loop() {
-  // Empty because FreeRTOS scheduler runs the task
+  if (IrReceiver.decode()) {
+    blink(); // Blink the LED when a signal is received
+    
+    Serial.print("Protocol: ");
+    Serial.println(IrReceiver.decodedIRData.protocol);
+
+    Serial.print("Address: ");
+    Serial.println(IrReceiver.decodedIRData.address, HEX);
+
+    Serial.print("Command: ");
+    Serial.println(IrReceiver.decodedIRData.command, HEX);
+
+    Serial.println("----------------");
+
+    IrReceiver.resume(); // Ready for next signal
+  }
 }
